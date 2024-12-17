4a581aea2d547f6e563a5446a8ee2fdb62feebd1
Guillermo Magallanes <memogm96@hotmail.com>
2024-12-17 14:55:23 -0600

    Update conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -6,13 +6,35 @@ try {
     echo "Conexión exitosa a Azure SQL Database.<br>";
 
     // Ejecuta una consulta de prueba
-    $query = "SELECT* FROM Empleados"; // Reemplaza 'TuTabla' con una tabla real
-    $stmt = $conn->query($query);
+   // $query = "SELECT* FROM Empleados"; // Reemplaza 'TuTabla' con una tabla real
+   // $stmt = $conn->query($query);
 
-    foreach ($stmt as $row) {
+   /* foreach ($stmt as $row) {
         print_r($row); // Muestra los resultados
-        echo "<br>";
+        echo "<br>";*/
+        $user = $_POST['username'];
+   $pass = $_POST['password'];
+    // Consulta para verificar las credenciales
+    $stmt = $conn->prepare("SELECT id FROM usuarios WHERE username =:username AND password =:password");
+    $stmt->bindParam(':username', $user);
+    $stmt->bindParam(':password', $pass);
+    $stmt->execute();
+    $res=$stmt->fetch(PDO::FETCH_ASSOC);
+   /*$res=$conn->query("SELECT id FROM usuarios where username = :username AND password = :password");
+    $res->bindParam(':username', $user);
+    $res->bindParam(':password', $pass);*/
+  
+    if ($res) {
+        echo "Inicio de sesión exitoso.";
+        header("Location: inicio.html");
+        exit();
+    } else {
+        //echo "Usuario o contraseña incorrectos.";
+      //echo "<p class='error'>¡Se ha producido un error! Por favor, inténtalo de nuevo.</p>";
+      echo "<script>alert('¡Error! Por favor, inténtalo de nuevo.');</script>";
+      exit();
     }
+   
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
