88bd95e7eec96eb50082e42a655b2c5534d717e0
Guillermo Magallanes <memogm96@hotmail.com>
2024-12-17 15:20:42 -0600

    Update conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -3,7 +3,7 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:memo96.database.windows.net,1433; Database = SafePass", "memo96", "Hmcrgl09");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Conexión exitosa a Azure SQL Database.<br>";
+    //echo "Conexión exitosa a Azure SQL Database.<br>";
 
     // Ejecuta una consulta de prueba
    // $query = "SELECT* FROM Empleados"; // Reemplaza 'TuTabla' con una tabla real
